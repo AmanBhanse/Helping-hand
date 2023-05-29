c33e40b43ab22095b561454d25ba1021ce4ce210
AmanBhanse <amanbhanse22@gmail.com>
2023-05-29 17:41:28 +0530

    Add item reducer to store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
-import membersReducer from "./components/AddMembersSublocation/membersSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import membersReducer from './components/AddMembersSublocation/membersSlice';
+import itemsReducer from './components/AddItemsSublocation/itemsSlice';
 
 export default configureStore({
   reducer: {
     membersContainer: membersReducer,
+    itemsStore: itemsReducer,
   },
 });
